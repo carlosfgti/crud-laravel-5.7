ee53fd858a2fc25ff3198c2f926ce34f2e936809
Carlos Ferreira <conta@especializati.com.br>
2018-09-06 10:06:56 -0300

    Relationships User Posts

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function posts()
+    {
+        $this->hasMany(Post::class);
+    }
 }
