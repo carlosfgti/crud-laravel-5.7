c583e3b35da5aeb298ea9fe34e2e483b258155fb
Carlos Ferreira <conta@especializati.com.br>
2018-09-06 10:03:40 -0300

    Define table posts

--- database/migrations/2018_09_06_100222_create_posts_table.php
+++ database/migrations/2018_09_06_100222_create_posts_table.php
@@ -15,6 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title')->unique();
+            $table->text('body');
             $table->timestamps();
         });
     }
