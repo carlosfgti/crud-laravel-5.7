641cbc804db629968c5dfd61d50b96c46dcc6fd6
Carlos Ferreira <conta@especializati.com.br>
2018-09-11 16:51:12 -0300

    show image view

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,9 +4,14 @@
 
     <h1>Posts</h1>
 
-    @foreach($posts as $post)
+    @foreach ($posts as $post)
     <article class="post">
         <a href="">
+            @if ($post->image)
+                <img src="{{ url("storage/posts/{$post->image}") }}" alt="{{ $post->title }}">
+            @else
+                <img src="{{ url('imgs/default.png') }}" alt="{{ $post->title }}">
+            @endif
             <h1>{{ $post->title }}</h1>
         </a>
     </article>
