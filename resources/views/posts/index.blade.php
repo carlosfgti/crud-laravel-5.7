636312e99fa42c7602d79608213e81186f7716ab
Carlos Ferreira <conta@especializati.com.br>
2018-09-12 17:28:49 -0300

    Show all posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,19 +2,44 @@
 
 @section('content')
 
-    <h1>Posts</h1>
+<h1>
+    Posts
+    <a href="{{ route('posts.create') }}" class="btn btn-primary">
+        <i class="fas fa-plus-square"></i>
+    </a>
+</h1>
 
-    @foreach ($posts as $post)
-    <article class="post">
-        <a href="{{ route('posts.show', $post->id) }}">
-            @if ($post->image)
-                <img src="{{ url("storage/posts/{$post->image}") }}" alt="{{ $post->title }}">
-            @else
-                <img src="{{ url('imgs/default.png') }}" alt="{{ $post->title }}">
-            @endif
-            <h1>{{ $post->title }}</h1>
-        </a>
-    </article>
-    @endforeach
+<ul class="media-list">
+    @forelse($posts as $post)
+    <li class="media">
+        <div class="pull-left">
+            @php
+                $pathImage = url('imgs/posts/default.png');
+
+                if ($post->image)
+                    $pathImage = url("storage/posts/{$post->image}");
+            @endphp
+            <img src="{{ $pathImage }}" alt="{{ $post->title }}" class="img-circle" style="max-width: 60px; margin: 10px;">
+        </div>
+        <div class="media-body">
+            <span class="text-muted pull-right">
+                <small class="text-muted">{{ $post->created_at->format('d/m/Y') }}</small>
+            </span>
+            <strong class="text-success">{{ '@' . $post->user->name }}</strong>
+            <p>
+                {{ $post->title }}
+                <br>
+                <a href="{{ route('posts.show', $post->id) }}">Detalhes</a> |
+                <a href="{{ route('posts.edit', $post->id) }}">Editar</a>
+            </p>
+        </div>
+    </li>
+    <hr>
+    @empty
+    <li class="media">
+        <p>Nenhum post cadastrado!</p>
+    </li>
+    @endforelse
+</ul>
 
 @endsection
