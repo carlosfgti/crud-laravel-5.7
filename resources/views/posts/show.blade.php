21c2ca1aa16760b134da6706e48b1be8be945d44
Carlos Ferreira <conta@especializati.com.br>
2018-09-14 16:31:57 -0300

    Show details post

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Detalhes do post <b>{{ $post->title }}</b></h1>
+
+@php
+    $pathImage = url('imgs/posts/default.png');
+
+    if ($post->image)
+        $pathImage = url("storage/posts/{$post->image}");
+@endphp
+<img src="{{ $pathImage }}" alt="{{ $post->title }}" class="img-circle">
+
+<p>{{ $post->body }}</p>
+
+<form action="{{ route('posts.destroy', $post->id) }}" method="post">
+    @csrf
+
+    <input type="hidden" name="_method" value="DELETE">
+
+    <button type="submit" class="btn btn-danger">Deletar</button>
+</form>
+
+@endsection
